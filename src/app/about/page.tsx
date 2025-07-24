f430cef3e1b70ed27fa62cd328f64c0c62b78edb
Manojpanda3 <manojpandawork@gmail.com>
2025-07-24 16:59:35 +0530

    adding about and contact page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+
+const AboutPage = () => {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">About Us</h1>
+          <p className="py-6">ScanMyBusiness is a website to scan your business for possible improvement and analyze your rivals for their best pointers which you can implement to improve your business.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,34 @@
+
+const ContactPage = () => {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Contact Us</h1>
+          <p className="py-6">Have a question? Fill out the form and we'll get back to you as soon as possible.</p>
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input type="text" placeholder="email" className="input input-bordered" />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Message</span>
+              </label>
+              <textarea className="textarea textarea-bordered" placeholder="Message"></textarea>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Send</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactPage;
