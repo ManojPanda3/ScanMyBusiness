15ff8fd0c1239245f7191c4cdc0d35a754da2140
Manojpanda3 <manojpandawork@gmail.com>
2025-07-24 16:59:22 +0530

    adding authentication to the app

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,32 @@
+
+import { PrismaClient } from '@prisma/client';
+import { NextResponse } from 'next/server';
+import bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+export async function POST(request: Request) {
+  const { email, password } = await request.json();
+
+  if (!email || !password) {
+    return NextResponse.json({ error: 'Missing fields' }, { status: 400 });
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    return NextResponse.json({ error: 'Invalid credentials' }, { status: 400 });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return NextResponse.json({ error: 'Invalid credentials' }, { status: 400 });
+  }
+
+  return NextResponse.json(user);
+}
---
+++ src/app/api/auth/signup/route.ts
@@ -0,0 +1,36 @@
+
+import { PrismaClient } from '@prisma/client';
+import { NextResponse } from 'next/server';
+import bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+export async function POST(request: Request) {
+  const { name, email, password } = await request.json();
+
+  if (!name || !email || !password) {
+    return NextResponse.json({ error: 'Missing fields' }, { status: 400 });
+  }
+
+  const exist = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (exist) {
+    return NextResponse.json({ error: 'User already exists' }, { status: 400 });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  return NextResponse.json(user);
+}
